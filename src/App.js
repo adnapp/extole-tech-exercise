0aa568544d26ec6f856a48140fc21aa13ca27e21
adnapp <adam.napps@gmail.com>
2021-05-19 14:33:48 -0400

    Working Product

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+
+    let [userEmail, setUserEmail] = useState("")
+
+    function handleSubmitEmail() {
+
+        let data = {
+            'email': userEmail
+        }
+        
+        fetch('https://extole-api.extole.io/api/v5/token', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                "Accept": "Application/json",
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('success got access token', data.access_token)
+            handleSetProfile(data.access_token)
+        })
+    }
+
+    function handleSetProfile(accessToken){
+
+        let data = {
+            "access_token": accessToken,
+            "email": "user@extole.com",
+        }
+
+        fetch('https://extole-api.extole.io/api/v4/me', {
+            method: 'POST', // or 'PUT'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (!data.status){
+                console.log(data.message)
+            }else{
+                console.log('Success set profile');
+                createShareLink(accessToken)
+            }
+        })
+    }
+
+    function createShareLink(accessToken){
+        console.log(accessToken)
+        let data = {
+            "label": "refer-a-friend"
+        }
+
+        fetch(`https://extole-api.extole.io/api/v6/me/shareables?access_token=${accessToken}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+           console.log("Share Link", data.link)
+        })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+        <input 
+            value={userEmail} 
+            onChange={(e)=> setUserEmail(e.target.value)}>
+
+        </input>
+        <button onClick={() => handleSubmitEmail()}>Execute</button>
+
+
+    </>
   );
 }
 
